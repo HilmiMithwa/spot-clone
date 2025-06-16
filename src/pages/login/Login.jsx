ff09a57d8ee01bf0711930fc68888775b17a8192
Hilmi Mithwa <hilmimithwa@gmail.com>
2025-06-16 21:15:33 +0700

    Adjusting login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 export default function Login() {
   const [nim, setNim] = useState('');
   const [password, setPassword] = useState('');
+  
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -39,28 +40,58 @@ export default function Login() {
       </div>
 
       {/*Login form container */}
-      <div id='login-form-container' className='w-1/2 min-h-screen flex items-center justify-center bg-danube-950 text-white font-roboto'>
-        <form onSubmit={handleLogin} className='bg-danube-900 rounded-xl mx-auto max-w-xl w-full px-9 pt-8 pb-8'>
-          <h1 className='font-semibold text-2xl mb-6 text-center'>Silahkan login terlebih dahulu</h1>
-          <div id='nim-container'>
-            <label>NIM:</label><br/>
-            <input className='bg-danube-800'></input>
-          </div>
-          <div id='password-container'>
-            <label>Password:</label><br/>
-            <input className='bg-danube-800 mb-10'></input>
-          </div>
+      <div
+  id='login-form-container'
+  className='w-1/2 min-h-screen flex items-center justify-center bg-danube-950 text-white font-roboto'
+>
+  <form
+    onSubmit={handleLogin}
+    className='bg-danube-900 rounded-xl mx-auto max-w-xl w-full px-9 pt-8 pb-8'
+  >
+    <h1 className='font-semibold text-2xl mb-6 text-center'>
+      Silahkan login terlebih dahulu
+    </h1>
 
-          <div id='button-container '>
-            <button className='bg-danube-950 mb-5 justify-center pt-3 pb-3 pl-50 pr-47'>Submit</button><br/>
-            <button className='bg-danube-950 justify-center pt-3 pb-3 pl-50 pr-50'>Clear</button>
-          </div>
-        </form>
+    {/* NIM Input */}
+    <div className='mb-4 flex justify-center'>
+      <div className='w-[397px]'>
+        <label className='font-light block mb-1'>NIM:</label>
+        <input
+          type='text'
+          className='bg-danube-800 rounded-[15px] h-[47px] w-full px-4'
+        />
       </div>
     </div>
-    </>
 
-    
-    
+    {/* Password Input */}
+    <div className='mb-4 flex justify-center'>
+      <div className='w-[397px]'>
+        <label className='font-light block mb-1'>Password:</label>
+        <input
+          type='password'
+          className='bg-danube-800 mb-4 rounded-[15px] h-[47px] w-full px-4'
+        />
+      </div>
+    </div>
+
+    {/* Tombol */}
+    <div id='button-container' className='flex flex-col gap-4 mt-4 w-[397px] mx-auto'>
+      <button
+        type='submit'
+        className='w-full bg-danube-950 text-white font-bold py-3 rounded-lg'
+      >
+        Masuk
+      </button>
+      <button
+        type='button'
+        className='w-full bg-danube-950 text-white font-bold py-3 rounded-lg'
+      >
+        Clear
+      </button>
+    </div>
+  </form>
+</div>
+    </div>
+    </>
   )
 }
